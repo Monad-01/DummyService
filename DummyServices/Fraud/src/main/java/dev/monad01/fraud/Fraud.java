cd310f68f893815c2dba8e576df3b6e27111f9aa
Linxerz <pruittj2000@Yahoo.com>
2023-10-05 00:56:03 -0500

    Reconfigured DB Containers

---
+++ DummyServices/Fraud/src/main/java/dev/monad01/fraud/Fraud.java
@@ -0,0 +1,13 @@
+package dev.monad01.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fraud {
+}
