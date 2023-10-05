fe5df2bce03c3efa3c05b40983307496f0c98497
Linxerz <pruittj2000@Yahoo.com>
2023-10-05 04:46:44 -0500

    IT CLICKED FINALLY!!!

--- DummyServices/Fraud/src/main/java/dev/monad01/fraud/FraudCheckService.java
+++ DummyServices/Fraud/src/main/java/dev/monad01/fraud/FraudCheckService.java
@@ -1,17 +1,15 @@
 package dev.monad01.fraud;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@AllArgsConstructor
 public class FraudCheckService {
 
     private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
-
-    public FraudCheckService(FraudCheckHistoryRepository fraudCheckHistoryRepository) {
-        this.fraudCheckHistoryRepository = fraudCheckHistoryRepository;
-    }
 
     public boolean isFraudulentCustomer(Integer customerId){
         fraudCheckHistoryRepository.save(
@@ -20,6 +18,8 @@ public class FraudCheckService {
                         .customerId(customerId)
                         .createdAt(LocalDateTime.now())
                         .build()
+
+
 
         );
 
