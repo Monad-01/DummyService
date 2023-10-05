e14eafcce266c0b3e6831c05cabb079d3bca829c
Linxerz <pruittj2000@Yahoo.com>
2023-10-05 04:55:22 -0500

    Small Refactoring on Customer

---
+++ DummyServices/Customer/src/main/java/dev/monad01/customer/CustomerConfig.java
@@ -0,0 +1,4 @@
+package dev.monad01.customer;
+
+public class CustomerConfig {
+}
--- DummyServices/Customer/src/main/java/dev/monad01/customer/CustomerService.java
+++ DummyServices/Customer/src/main/java/dev/monad01/customer/CustomerService.java
@@ -1,9 +1,13 @@
 package dev.monad01.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService(CustomerRepostitory customerRepostitory) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepostitory customerRepostitory;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
