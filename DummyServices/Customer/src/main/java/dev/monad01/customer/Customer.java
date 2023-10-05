9d9be5331abe63420e971d2708ae5ca6e008b431
Linxerz <pruittj2000@Yahoo.com>
2023-10-04 23:53:02 -0500

    Hibernate Issues Resolved

--- DummyServices/Customer/src/main/java/dev/monad01/customer/Customer.java
+++ DummyServices/Customer/src/main/java/dev/monad01/customer/Customer.java
@@ -9,21 +9,23 @@ import javax.persistence.*;
 @Data
 @Builder
 @Entity
+@Table(name="customer")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
 
     @Id
-    @SequenceGenerator(
-            name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
-    )
     @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence"
+            strategy = GenerationType.IDENTITY
     )
     private Integer id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
 }
--- DummyServices/Customer/src/main/java/dev/monad01/customer/CustomerService.java
+++ DummyServices/Customer/src/main/java/dev/monad01/customer/CustomerService.java
@@ -3,13 +3,15 @@ package dev.monad01.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepostitory customerRepostitory) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+
+        customerRepostitory.save(customer);
 
     }
 }
