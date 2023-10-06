206b738a02b651f0d5f579f7296e5bc371718a4e
Linxerz <pruittj2000@Yahoo.com>
2023-10-06 03:18:20 -0500

    yh

--- DummyServices/Customer/src/main/java/dev/monad01/customer/CustomerService.java
+++ DummyServices/Customer/src/main/java/dev/monad01/customer/CustomerService.java
@@ -2,12 +2,14 @@ package dev.monad01.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepostitory customerRepostitory;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -15,7 +17,13 @@ public class CustomerService {
                 .email(request.email())
                 .build();
 
-        customerRepostitory.save(customer);
+        customerRepostitory.saveAndFlush(customer);
+
+        restTemplate.getForObject(
+                "http:localhost:8081/api/v1/fraud-check{customerId}",
+
+        )
+
 
     }
 }
